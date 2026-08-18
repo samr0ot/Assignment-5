73920c784ccb5dd532b0b67ee19ec9369eea4f97
samr0ot <samyamkunwar9@gmail.com>
2026-08-18 21:25:26 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ const resetBtn = document.getElementById("resetBtn");
 
 const defaultColor = "#4e73df";
 
-// change box color whenever the color input changes
 colorInput.addEventListener("input", function () {
   colorBox.style.backgroundColor = colorInput.value;
 });
 
-// reset box back to the default color
 resetBtn.addEventListener("click", function () {
   colorBox.style.backgroundColor = defaultColor;
   colorInput.value = defaultColor;
