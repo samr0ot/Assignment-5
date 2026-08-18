da8139e7dce6840d26f3ff6c36ecf288b897574a
samr0ot <samyamkunwar9@gmail.com>
2026-08-18 21:26:10 +0545

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,5 +1,3 @@
-// Task 3: Callback Problem - Callback Hell
-
 function getUser(id, callback) {
   setTimeout(() => {
     console.log("User fetched");
@@ -21,9 +19,6 @@ function getComments(post, callback) {
   }, 1000);
 }
 
-// Nested callbacks - this is "callback hell":
-// each step is only reachable by nesting inside the previous one,
-// making the code harder to read as more steps are added.
 getUser(1, function (user) {
   console.log("User:", user);
 
